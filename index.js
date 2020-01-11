240dbe560fbf262e2a639e9dce374a7e35a95bbc
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-11 14:12:57 -0800

    Setup Express server

---
+++ index.js
@@ -0,0 +1,24 @@
+// Require Dependencies
+const express = require("express");
+const fs = require("fs");
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Setup global notes variable
+fs.readFile("./db/db.json","json",function(data) {
+    var notes = data;
+});
+
+//Require routes file
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
