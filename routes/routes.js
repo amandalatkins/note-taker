a8a7e7576f44aa1787418c54a30c4fac647c2846
Amanda Atkins <amanda@digitalrainstorm.com>
2020-01-11 16:06:15 -0800

    Modify JSON structure and modify route functionality to reflect new structure

--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,6 @@ module.exports = app => {
     readFileAsync("db/db.json","utf8").then(function(data) {
         // console.log("Reading db file");
         var notes = JSON.parse(data);
-        var uniqueId = notes.length;
     
         // Read the db.json file and return all saved notes as JSON.
         app.get("/api/notes", function(req, res) {
@@ -20,7 +19,7 @@ module.exports = app => {
         // Receives a new note, adds it to db.json, then returns the new note
         app.post("/api/notes", function(req, res) {
             let newNote = req.body;
-            notes[uniqueId++] = newNote;
+            notes.push(newNote);
 
             updateDb();
 
@@ -35,7 +34,7 @@ module.exports = app => {
 
         // Deletes a note with specific id
         app.delete("/api/notes/:id", function(req, res) {
-            delete notes[req.params.id];
+            notes.splice(req.params.id, 1);
             updateDb();
             console.log("Deleted note with id "+req.params.id);
         });
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
 //Require routes file
 require('./routes/routes')(app);
